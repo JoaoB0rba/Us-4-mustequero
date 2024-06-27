36431f605aa8c568ec59b48b06d402e8b614f257
JPezinBorba <jp130766@gmail.com>
2024-06-27 15:55:22 -0300

    organizado

---
+++ codigo/conexao.php
@@ -0,0 +1,7 @@
+<?php
+     $servidor = "db";
+     $usuario = "root";
+     $pasword = "321";
+     $banco ="4mustequeiro";
+     $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+?>
