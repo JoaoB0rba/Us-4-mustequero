bf4eceb9f79d2aab5179310467e8df7158baf771
Joao_b0rba <148866574+JoaoB0rba@users.noreply.github.com>
2024-11-08 08:05:10 -0300

    teste

--- codigo/conexao.php
+++ codigo/conexao.php
@@ -1,8 +1,9 @@
-<?php
+<?php  
     $servidor = 'db';
     $usuario = 'root';
     $senha = '123';
     $banco = 'bancoveiculos';
 
+    
     $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 ?>
