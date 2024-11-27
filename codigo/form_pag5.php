548e997f65551e84290b5361841c885edd5f586e
carola1901 <carolinirn07@gmail.com>
2024-11-27 14:44:52 -0300

    DEU CERTO KRL

--- codigo/form_pag5.php
+++ codigo/form_pag5.php
@@ -3,13 +3,12 @@ require_once "conexao.php";
 require_once "operacoes.php";
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['precokm']) && isset($_POST['tipopag']) && isset($_POST['valorTotal'])) {
-    $idaluguel = $_POST['idaluguel'];
-    $precokm = $_POST['precokm'];
-    $tipopag = $_POST['tipopag'];
-    $valorTotal = $_POST['valorTotal'];
-    $kmFinal = $_POST['kmfinal'];  // Quilometragem final para cada veículo
-    $veiculos = $_POST['veiculos'];  // IDs dos veículos
+    $idaluguel = $_GET['idaluguel'];
+    $precokm = $_GET['precokm'];
+    $tipopag = $_GET['tipopag'];
+    $valorTotal = $_GET['valorTotal'];
+    $kmFinal = $_GET['kmfinal'];  // Quilometragem final para cada veículo
+    $veiculos = $_GET['veiculos'];  // IDs dos veículos
 
     // Atualiza a quilometragem atual do veículo (passando cada veículo individualmente)
     foreach ($kmFinal as $idVeiculo => $kmFinalValue) {
@@ -25,9 +24,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['precokm']) && isset($
     // Redireciona para a tela inicial ou uma página de sucesso
     header('Location: telainicial.html');
     exit();
-} else {
-    // Se os dados não foram enviados corretamente, redireciona para a página de lançamento do pagamento
-    header('Location: form_pag4.php?idaluguel=' . $_POST['idaluguel']);
-    exit();
-}
+
 ?>
